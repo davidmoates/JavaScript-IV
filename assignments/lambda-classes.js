de042f506e2d69a4a4b56f238cdca66073cd45d5
David Moates <david.m.moates@gmail.com>
2019-08-15 23:28:33 -0500

    some of classes done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,37 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(pAtts) {
+    this.name = pAtts.name;
+    this.age = pAtts.age;
+    this.location = pAtts.location;
+  }
+  speak() {
+    return `Hello, My name is ${this.name}, and I am from ${this.location}.`;
+  }
+}
+
+class Instructor extends Person {
+  constructor(iAtts) {
+    super(iAtts);
+    this.specialty = iAtts.specialty;
+    this.favLanguage = iAtts.favLanguage;
+    this.catchPhrase = iAtts.catchPhrase;
+  }
+  demo(subject) {
+    return `Today we are learning about ${subject}.`;
+  }
+  grade(Student, subject) {
+    `${Student.name} receives a perfect score on ${subject}.`;
+  }
+}
+
+class Student extends Person {
+  constructor(sAtts) {
+    super(sAtts);
+    this.previousBackground = sAtts.previousBackground;
+    this.className = sAtts.className;
+    this.favSubjects = sAtts.favSubjects;
+    this.listsSubjects = sAtts.listsSubjects;
+  }
+}
